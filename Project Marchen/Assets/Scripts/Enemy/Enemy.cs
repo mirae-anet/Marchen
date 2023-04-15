96ae0679b18079d9cec2bd8a52035b1a457a0d32
Kim-Yundong-KYD <kimyd123456@gmail.com>
2023-04-15 22:37:30 +0900

    v0.13
    Enemy (애너미 AI) 스크립트, Missile (미사일 회전) 스크립트, Item (Item Type 선언, 아이템 회전) 스크립트 추가

---
+++ Project Marchen/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public enum Type {A, B, C};
+    public Type enemyType;
+    public int maxHealth;
+    public int curHealth;
+    public Transform target;
+    public BoxCollider meleeArea;
+    public GameObject bullet;
+    public bool isChase;
+    public bool isAttack;
+
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    Material mat;
+    NavMeshAgent nav;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        Invoke("ChaseStart", 2);
+    }
+
+    void Update()
+    {
+        if (nav.enabled)
+        {
+            nav.SetDestination(target.position);
+            nav.isStopped = !isChase;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+        Targeting();
+    }
+
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+
+    void FreezeVelocity()
+    {
+        if (isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void Targeting()
+    {
+        float targetRadius = 0;
+        float targetRange = 0;
+
+        switch (enemyType)
+        {
+            case Type.A:
+                targetRadius = 1.5f;
+                targetRange = 3f;
+                break;
+
+            case Type.B:
+                targetRadius = 1f;
+                targetRange = 12f;
+                break;
+
+            case Type.C:
+                targetRadius = 0.5f;
+                targetRange = 25f;
+                break;
+        }
+
+        RaycastHit[] rayHits =
+            Physics.SphereCastAll(transform.position,
+                                  targetRadius,
+                                  transform.forward, 
+                                  targetRange,
+                                  LayerMask.GetMask("Player"));
+
+        if (rayHits.Length > 0 && !isAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        isChase = false;
+        isAttack = true;
+        anim.SetBool("isAttack", true);
+
+        switch (enemyType)
+        {
+            case Type.A:
+                yield return new WaitForSeconds(0.2f);
+                meleeArea.enabled = true;
+
+                yield return new WaitForSeconds(1f);
+                meleeArea.enabled = false;
+
+                yield return new WaitForSeconds(1f);
+                break;
+
+            case Type.B:
+                yield return new WaitForSeconds(0.1f);
+                rigid.AddForce(transform.forward * 20, ForceMode.Impulse);
+                meleeArea.enabled = true;
+
+                yield return new WaitForSeconds(0.5f);
+                rigid.velocity = Vector3.zero;
+                meleeArea.enabled = false;
+
+                yield return new WaitForSeconds(2f);
+                break;
+
+            case Type.C:
+                yield return new WaitForSeconds(0.5f);
+                GameObject instantBullet = Instantiate(bullet, transform.position, transform.rotation);
+                Rigidbody rigidBullet = instantBullet.GetComponent<Rigidbody>();
+                rigidBullet.velocity = transform.forward * 20;
+
+                yield return new WaitForSeconds(2f);
+
+                break;
+        }
+
+        isChase = true;
+        isAttack = false;
+        anim.SetBool("isAttack", false);
+    }
+}
---
+++ Project Marchen/Assets/Scripts/Enemy/Missile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.right * 30 * Time.deltaTime);
+    }
+}
