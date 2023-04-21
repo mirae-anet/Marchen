064aeaa90a39ecb94e7ba575005b4d87230aa140
tired_i <ramen4598@naver.com>
2023-04-22 01:34:45 +0900

    RPC to server

--- Project Marchen/Assets/Scripts/Network/NetworkPlayer.cs
+++ Project Marchen/Assets/Scripts/Network/NetworkPlayer.cs
@@ -24,6 +24,9 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
             //Disable main camera
             Camera.main.gameObject.SetActive(false);
 
+            //set nickname
+            RPC_SetNickName(PlayerPrefs.GetString("PlayerNickname"));
+
             Debug.Log("Spawned local player");
         }
         else
@@ -58,5 +61,12 @@ public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
     {
         Debug.Log($"Nickname changed for player to {nickName} for player {gameObject.name}");
         playerNickNameTM.text = nickName.ToString();
+    }
+    //from client to server
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_SetNickName(string nickName, RpcInfo info = default)
+    {
+        Debug.Log($"[RPC] SetNickName {nickName}");
+        this.nickName = nickName;
     }
 }
