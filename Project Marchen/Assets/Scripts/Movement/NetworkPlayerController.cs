b5c203bcde783a0cd6b9d60294cb54a59e9457be
pickup3415 <slajghrl@naver.com>
2023-05-24 04:07:12 +0900

    merge후 오류 수정

--- Project Marchen/Assets/Scripts/Movement/NetworkPlayerController.cs
+++ Project Marchen/Assets/Scripts/Movement/NetworkPlayerController.cs
@@ -263,6 +263,7 @@ public class NetworkPlayerController : NetworkBehaviour
                 
                 if (string.IsNullOrEmpty(inputText))
                 {
+                    isChatting = false;
                     return;
                 }
 
