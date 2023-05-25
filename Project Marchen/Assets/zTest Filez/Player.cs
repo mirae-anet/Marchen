f4b4c1fcc22662424a90b1fc5bcc3b0f7d88698f
Kim-Yundong-KYD <kimyd123456@gmail.com>
2023-05-25 21:02:11 +0900

    스크립트 추가

--- Project Marchen/Assets/zTest Filez/Player.cs
+++ Project Marchen/Assets/zTest Filez/Player.cs
@@ -6,23 +6,32 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    private bool wDown;
     private float hAxis;
     private float vAxis;
 
     Vector3 moveVec;
 
-    void Start()
+    Animator anim;
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
     }
 }
