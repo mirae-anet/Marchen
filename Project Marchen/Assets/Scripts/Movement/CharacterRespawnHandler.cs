b7c0c34d034e38b53e032cbcc08ae54e9a3a8268
tired_i <ramen4598@naver.com>
2023-06-05 04:16:37 +0900

    BugFix : 낙사 기준 재설정

--- Project Marchen/Assets/Scripts/Movement/CharacterRespawnHandler.cs
+++ Project Marchen/Assets/Scripts/Movement/CharacterRespawnHandler.cs
@@ -37,27 +37,8 @@ public class CharacterRespawnHandler : NetworkBehaviour
     public override void FixedUpdateNetwork()
     {
         if(Object.HasStateAuthority)
-        {
             if(isRespawnRequested)
-            {
                 Respawn();
-                return;
-            }               
-
-            //Don't update the client positon when they are dead
-            if(hpHandler.GetIsDead())
-                return;
-        }
-
-        //get NetworkInputData from Client
-        if(GetInput(out NetworkInputData networkInputData))
-        {
-            if(!Object.HasStateAuthority)
-                return;
-
-            CheckFallRespawn();
-
-        }
     }
 
     public void RequestRespawn()
@@ -70,19 +51,6 @@ public class CharacterRespawnHandler : NetworkBehaviour
         transform.position = Utils.GetRandomSpawnPoint(spawnPoint);
         hpHandler.OnRespawned();
         isRespawnRequested = false;
-    }
-
-    void CheckFallRespawn()
-    {
-        if(transform.position.y < -12)
-        {
-            if(Object.HasStateAuthority)
-            {
-                Debug.Log($"{Time.time} Respawn due to fall outside of map at position {transform.position}");
-
-                hpHandler.OnTakeDamage(networkPlayer.nickName.ToString(),(byte)255,transform.position);
-            }
-        }
     }
 
     public void ChangeSpawnPoint(Vector3 newSpawnPoint)
