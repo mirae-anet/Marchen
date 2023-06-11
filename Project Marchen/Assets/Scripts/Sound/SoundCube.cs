82479c96a7711fbdbfed2a92d5b07cafdd574135
Arkru <parkkr1204@naver.com>
2023-06-11 10:19:38 +0900

    배경음악 큐브 수정

--- Project Marchen/Assets/Scripts/Sound/SoundCube.cs
+++ Project Marchen/Assets/Scripts/Sound/SoundCube.cs
@@ -7,13 +7,19 @@ public class SoundCube : MonoBehaviour
     // 인스펙터
     [Header("오브젝트 연결")]
     [SerializeField]
-    private AudioSource audioSource;
+    private AudioSource StartAudioSource;
+    [SerializeField]
+    private AudioSource StopAudioSource;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            audioSource.Play();
+            if (StopAudioSource != null)         
+                StopAudioSource.Stop();
+
+            if (StartAudioSource != null)
+                StartAudioSource.Play();
         }
     }
 }
