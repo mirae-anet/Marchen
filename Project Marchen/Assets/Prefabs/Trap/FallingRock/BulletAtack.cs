29e2bd059074f724c3dc675eb8f4d904ab8fb7e5
pickup3415 <slajghrl@naver.com>
2023-10-16 02:39:15 +0900

    tag > compareTag변경, Ground 적중시 분리

--- Project Marchen/Assets/Prefabs/Trap/FallingRock/BulletAtack.cs
+++ Project Marchen/Assets/Prefabs/Trap/FallingRock/BulletAtack.cs
@@ -7,15 +7,19 @@ public class BulletAtack : NetworkBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" || other.tag == "Ground")
+        if (other.CompareTag("Player") || other.CompareTag("Ground"))
         {
+            if(other.CompareTag("Ground"))
+            {
+                Destroy(gameObject);
+            }
+
             HPHandler hpHandler = other.transform.root.GetComponent<HPHandler>();
             int damageAmount = 50;
             if (hpHandler != null)
             {
                 Destroy(gameObject);
                 hpHandler.OnTakeDamage(other.transform.name, damageAmount, other.transform.position);
-                
             }
         }
     }
