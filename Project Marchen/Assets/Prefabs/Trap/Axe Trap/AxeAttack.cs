e570cdb2660f7b90f99ed56cdb166f5d72dae08b
pickup3415 <slajghrl@naver.com>
2023-10-15 22:41:10 +0900

    도끼 데미지 수정

--- Project Marchen/Assets/Prefabs/Trap/Axe Trap/AxeAttack.cs
+++ Project Marchen/Assets/Prefabs/Trap/Axe Trap/AxeAttack.cs
@@ -7,15 +7,24 @@ using UnityEngine;
 public class AxeAttack : NetworkBehaviour
 {
 
+    /*public void KnockBack(Vector3 AttackPostion)
+    {
+        Vector3 reactDir = (transform.position - AttackPostion).normalized;
+
+        rigid.AddForce(Vector3.up * 25f, ForceMode.Impulse);
+        rigid.AddForce(reactDir * 10f, ForceMode.Impulse);
+    }*/
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             HPHandler hpHandler = other.transform.root.GetComponent<HPHandler>();
-            int damageAmount = 30;
+            int damageAmount = 50;
             if (hpHandler != null)
             {
                 hpHandler.OnTakeDamage(other.transform.name, damageAmount, other.transform.position);
+               
             }
         }
     } 
