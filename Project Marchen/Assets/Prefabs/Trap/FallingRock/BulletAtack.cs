3613eb3a05394f8834c1220ade66ee51a7b97464
pickup3415 <slajghrl@naver.com>
2023-10-16 02:21:57 +0900

    낙석 동기화 테스트용

--- Project Marchen/Assets/Prefabs/Trap/FallingRock/BulletAtack.cs
+++ Project Marchen/Assets/Prefabs/Trap/FallingRock/BulletAtack.cs
@@ -1,24 +1,22 @@
-using Fusion;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class AxeAttack : NetworkBehaviour
+public class BulletAtack : NetworkBehaviour
 {
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" || other.tag == "Ground")
         {
             HPHandler hpHandler = other.transform.root.GetComponent<HPHandler>();
             int damageAmount = 50;
             if (hpHandler != null)
             {
+                Destroy(gameObject);
                 hpHandler.OnTakeDamage(other.transform.name, damageAmount, other.transform.position);
-               
+                
             }
         }
-    } 
+    }
 }
-
